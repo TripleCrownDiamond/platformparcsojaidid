2c87e94fc50071aaacaed0e6a45a234536663ce8
TripleCrownDiamond <gagbahungba2019@gmail.com>
2024-09-24 17:18:57 +0200

    removed ui-btn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,6 @@ import Testimonials from "@/components/Testimonials/Testimonials";
 import Contact from "@/components/Contact/Contact";
 import About from "@/components/About/About";
 import Footer from "@/components/Footer/Footer";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
