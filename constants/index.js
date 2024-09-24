0e363ff41f51082293b1e0a70f8de65f37b46443
TripleCrownDiamond <gagbahungba2019@gmail.com>
2024-09-24 17:54:35 +0200

    fixed some buggs

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,18 @@ import { auth } from "@clerk/nextjs/server";
 import imageFormats from "./imageFormats";
 
 export const baseUrl = "https://efficient-apparel-56013b6060.strapiapp.com";
-export  const { userId } = auth();
+
+import { getAuth } from '@clerk/tanstack-start/server';
+
+export async function fetchUserId(request) {
+  const { userId } = await getAuth(request);
+
+  if (!userId) {
+    throw new Error('User not authenticated');
+  }
+
+  return userId;
+}
 
 // Fonction pour obtenir l'URL de l'image selon les formats disponibles
 export const getImageUrl = (image) => {
